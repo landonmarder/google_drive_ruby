232f73834614429bd6fbb6ca3f674103ea736f46
landonmarder <landon.marder@gmail.com>
2014-03-21 17:20:55 -0400

    takes google spreadsheet and writes into a json

--- google_guide.rb
+++ google_guide.rb
@@ -1,5 +1,21 @@
 require "rubygems"
 require "google_drive"
+require 'json'
 require 'pry'
 
-session = GoogleDrive.login("landon.marder@gmail.com", )
+password = File.open(".secret", "r").read
+password.gsub!("\n", '')
+
+session = GoogleDrive.login("landon.marder@gmail.com", password)
+
+rows = session.spreadsheet_by_key('0AornRwZY3X2FdDdpRUhnMG1wMWJOdEtGTGZDbmNTYkE').worksheets[0].rows
+companies = Hash.new({})
+
+rows.each do |row|
+  companies["#{row[0]}"] = { name: row[0], url: row[1], category: row[2] }
+end
+
+# Delete the header
+companies.delete("Company")
+
+File.open('data/spreadsheet.json', 'w') { |f| f << companies.to_json }
