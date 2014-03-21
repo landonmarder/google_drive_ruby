75047d318114fd5d95f6957b547cf222991654db
landonmarder <landon.marder@gmail.com>
2014-03-21 17:39:40 -0400

    add middleman comments

--- google_guide.rb
+++ google_guide.rb
@@ -3,14 +3,18 @@ require "google_drive"
 require 'json'
 require 'pry'
 
+# Password is located ina .secret file
 password = File.open(".secret", "r").read
 password.gsub!("\n", '')
 
+# Authenticate with Google
 session = GoogleDrive.login("landon.marder@gmail.com", password)
 
+# Get the spreadsheet you want
 rows = session.spreadsheet_by_key('0AornRwZY3X2FdDdpRUhnMG1wMWJOdEtGTGZDbmNTYkE').worksheets[0].rows
 companies = Hash.new({})
 
+# Convert spreadsheet to hash
 rows.each do |row|
   companies["#{row[0]}"] = { name: row[0], url: row[1], category: row[2] }
 end
@@ -18,4 +22,10 @@ end
 # Delete the header
 companies.delete("Company")
 
-File.open('data/spreadsheet.json', 'w') { |f| f << companies.to_json }
+# Write hash into the file as json
+File.open('data/companies.json', 'w') { |f| f << companies.to_json }
+
+# How to access the json file in middleman
+# <% data.companies.keys.each do |company| %>
+#   <%= data.companies[company][:name]%>
+# <% end %>
