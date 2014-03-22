ed11ca7f10c870cd2daf9e8edc71ba6abbd77aa2
landonmarder <landon.marder@gmail.com>
2014-03-22 15:58:02 -0400

    remove personal email for sample email, add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+Using Google Drive as a database for a static Middleman App
+============================================================
+
+Solution to 'DRYing' up a middleman app and allowing 'non-programmers' to maintain a one table database on a google drive spreadsheet.
+
+Create a rake task in your middleman app to 'update' your database for your middleman views and then deploy/build Middleman.
--- google_guide.rb
+++ google_guide.rb
@@ -8,10 +8,13 @@ password = File.open(".secret", "r").read
 password.gsub!("\n", '')
 
 # Authenticate with Google
-session = GoogleDrive.login("landon.marder@gmail.com", password)
+session = GoogleDrive.login("your-email@gmail.com", password)
 
 # Get the spreadsheet you want
-rows = session.spreadsheet_by_key('0AornRwZY3X2FdDdpRUhnMG1wMWJOdEtGTGZDbmNTYkE').worksheets[0].rows
+# Spreadsheet URL: https://docs.google.com/spreadsheet/ccc?key=0AornRwZY3X2FdDdpRUhnMG1wMWJOdEtGTGZDbmNTYkE&usp=drive_web#gid=0
+
+google_spreadsheet_key = '0AornRwZY3X2FdDdpRUhnMG1wMWJOdEtGTGZDbmNTYkE&usp'
+rows = session.spreadsheet_by_key(google_spreadsheet_key).worksheets[0].rows
 
 companies = Array.new
 
@@ -23,7 +26,7 @@ end
 # Write array into the file as json
 File.open('data/companies.json', 'w') { |f| f << companies.to_json }
 
-# How to access the json file in middleman
+# How to access the json file in middleman (file located in data/companies.json in your middleman app)
 # <% data.companies.each do |company| %>
 #    <%= company[:name]%>
 # <% end %>
